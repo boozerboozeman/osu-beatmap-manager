39763af54dda1b7362f34c757cade4d4c5a3e77a
boozerboozeman <theobertin75@icloud.com>
2021-05-18 14:12:37 +0200

    first commit

---
+++ osu-beatmap-manager/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace osu_beatmap_manager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(253, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Installation";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Binary status:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Beatmaps found:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Location = new System.Drawing.Point(13, 120);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(253, 100);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Backup";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(237, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "backup beatmaps";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(133, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Waiting for user interaction";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 384);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "osu beatmap manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ osu-beatmap-manager/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Compression;
+
+namespace osu_beatmap_manager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public bool detectBinary()
+        {
+            string directory = @"C:\Users\" + Environment.UserName + "\\AppData\\Local\\osu!\\osu!.exe";
+            
+            if (File.Exists(directory)){
+                return true;
+            }
+            else {
+                return false;
+            }
+
+        }
+
+        public int findBeatmaps(string directory)
+        {
+            string[] files = Directory.GetFileSystemEntries(directory, "*");
+            return files.Length;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (detectBinary()){
+
+            
+                label1.Text += "Detected";
+                label2.Text += findBeatmaps( @"C:\Users\" + Environment.UserName + "\\AppData\\Local\\osu!\\Songs\\").ToString();
+            }
+            else { 
+                label1.Text += "Couldn't detect your install of osu.";
+            }
+        }
+
+        public void backupMaps(string[] maps)
+        {
+          
+            foreach(string map in maps){
+                ZipFile.CreateFromDirectory(map, map + ".osz");
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] files = Directory.GetFileSystemEntries(@"C:\Users\" + Environment.UserName + "\\AppData\\Local\\osu!\\Songs\\", "*");
+            label3.Text = "Backing up beatmaps";
+            backupMaps(files);
+        }
+    }
+}
